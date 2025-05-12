1ba4a75cc366f08ef00d75ff742285143069fa20
mike castleman <m@mlcastle.net>
2025-05-12 19:00:12 -0400

    on paste into multi text box, find and extract coords

--- src/GameParams.tsx
+++ src/GameParams.tsx
@@ -32,6 +32,8 @@ const BasicInput = () => {
   );
 };
 
+const COORDS_RE = /[+-]?[0-9]+(?:\.?[0-9]+)?[,\s]+[+-]?[0-9]+(?:\.?[0-9]+)?/g;
+
 const MultiInput = () => {
   const dispatch = useAppDispatch();
   const text = useAppSelector((state) => state.game.multiText);
@@ -42,11 +44,31 @@ const MultiInput = () => {
       },
       [dispatch]
     );
+  const onPaste: React.ClipboardEventHandler<HTMLTextAreaElement> =
+    React.useCallback(
+      (evt) => {
+        const data = evt.clipboardData.getData('text/plain');
+        let results: string[] = [];
+        for (const result of data.matchAll(COORDS_RE)) {
+          results.push(result[0]);
+        }
+        if (results.length > 0) {
+          evt.preventDefault();
+          dispatch(setMulti(results.join('\n')));
+        }
+      },
+      [dispatch]
+    );
   return (
     <label>
       Target coordinates (one per line):
       <br />
-      <textarea name="multi" value={text} onChange={onChange} />
+      <textarea
+        name="multi"
+        value={text}
+        onChange={onChange}
+        onPaste={onPaste}
+      />
     </label>
   );
 };
