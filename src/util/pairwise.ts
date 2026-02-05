fb525633a0b52210906b5665f089556404db6e02
mike castleman <m@mlcastle.net>
2026-02-05 12:14:41 -0500

    allow setting minimum distance

--- src/util/pairwise.ts
+++ src/util/pairwise.ts
@@ -1,4 +1,4 @@
-import type { Coord } from '@turf/helpers';
+import { type Coord, earthRadius } from '@turf/helpers';
 import { getCoords } from '@turf/invariant';
 import type {
   Feature,
@@ -23,8 +23,8 @@ const pointToVec3 = (p: Coord): Vec3 => {
 
 interface Node {
   idx: readonly number[];
-  left: Node | null;
-  right: Node | null;
+  left?: Node;
+  right?: Node;
   c: Vec3;
   R: number;
   isLeaf: boolean;
@@ -126,7 +126,7 @@ const buildBallTree = (
   const { c, R } = computeCenterAndRadius(points, idxList);
 
   if (idx.length <= LEAF) {
-    return { idx, c, R, left: null, right: null, isLeaf: true };
+    return { idx, c, R, isLeaf: true };
   }
 
   const { L: left, R: right } = splitIndices(points, idx);
@@ -164,6 +164,32 @@ const upperBound = (A: Node, B: Node, T: Vec3) => {
   }
 };
 
+interface MinParams {
+  cosThetaMin?: number;
+  chordMin?: number;
+}
+
+const computeMinParams = (minDistance?: number): MinParams => {
+  if (typeof minDistance === 'number' && minDistance > 0) {
+    const thetaMin = (minDistance * 1000) / earthRadius;
+    return {
+      cosThetaMin: Math.cos(thetaMin),
+      chordMin: 2 * Math.sin(thetaMin / 2),
+    };
+  } else {
+    return {};
+  }
+};
+
+const allPairsTooClose = (A: Node, B: Node, chordMin?: number): boolean => {
+  if (chordMin === undefined) {
+    return false;
+  } else {
+    const centerDist = norm(subVec3(A.c, B.c));
+    return centerDist + A.R + B.R < chordMin;
+  }
+};
+
 interface State {
   bestScore: number;
   bestI: number;
@@ -175,33 +201,38 @@ export default class PairwiseComputer<P = GeoJsonProperties> {
     s1: FeatureCollection<Point, P>,
     s2?: FeatureCollection<Point, P>
   ) {
-    this.#s1 = s1;
-    this.#s2 = s2 ?? s1;
+    this.s1 = s1;
+    this.s2 = s2 ?? s1;
     const S1 = s1.features.map((f) => pointToVec3(f.geometry.coordinates));
     const S2 = s2?.features?.map((f) => pointToVec3(f.geometry.coordinates));
-    this.#S1 = S1;
-    this.#S2 = S2 ?? S1;
+    this.S1 = S1;
+    this.S2 = S2 ?? S1;
     this.root1 = buildBallTree(S1, Array.from(Array(S1.length).keys()));
     this.root2 = S2
       ? buildBallTree(S2, Array.from(Array(S2.length).keys()))
       : this.root1;
   }
 
-  evalLeafPair = (
+  private evalLeafPair = (
     A: Node,
     B: Node,
     T: Vec3,
-    { bestScore, bestI, bestJ }: State
+    { bestScore, bestI, bestJ }: State,
+    cosThetaMin?: number
   ): State => {
     for (const i of A.idx) {
       for (const j of B.idx) {
-        const s = addVec3(this.#S1[i], this.#S2[j]);
+        const s = addVec3(this.S1[i], this.S2[j]);
         const sn = norm(s);
         if (sn < 1e-12) {
           continue;
         }
         const score = dot(T, s) / sn;
-        if (score > bestScore) {
+        if (
+          score > bestScore &&
+          (cosThetaMin === undefined ||
+            dot(this.S1[i], this.S2[j]) <= cosThetaMin)
+        ) {
           bestScore = score;
           bestI = i;
           bestJ = j;
@@ -212,9 +243,12 @@ export default class PairwiseComputer<P = GeoJsonProperties> {
   };
 
   bestMidpointPair = (
-    t: Vec3 | Point | Feature<Point, any>
+    t: Vec3 | Point | Feature<Point, any>,
+    minDistance?: number
   ): [Feature<Point, P>, Feature<Point, P>] => {
     const T: Vec3 = 'type' in t ? pointToVec3(t) : t;
+
+    const { cosThetaMin, chordMin } = computeMinParams(minDistance);
 
     let leaf1 = this.root1;
     while (!leaf1.isLeaf) {
@@ -225,11 +259,17 @@ export default class PairwiseComputer<P = GeoJsonProperties> {
       leaf2 = leaf2.left!;
     }
 
-    let state = this.evalLeafPair(leaf1, leaf2, T, {
-      bestScore: -1.0,
-      bestI: -1,
-      bestJ: -1,
-    });
+    let state = this.evalLeafPair(
+      leaf1,
+      leaf2,
+      T,
+      {
+        bestScore: -1.0,
+        bestI: -1,
+        bestJ: -1,
+      },
+      cosThetaMin
+    );
 
     const pq = new Heap<PairItem>((a, b) => b.ub - a.ub);
     let ub0 = upperBound(this.root1, this.root2, T);
@@ -242,8 +282,12 @@ export default class PairwiseComputer<P = GeoJsonProperties> {
         continue;
       }
 
+      if (allPairsTooClose(A, B, chordMin)) {
+        continue;
+      }
+
       if (A.isLeaf && B.isLeaf) {
-        state = this.evalLeafPair(A, B, T, state);
+        state = this.evalLeafPair(A, B, T, state, cosThetaMin);
         continue;
       }
 
@@ -285,13 +329,13 @@ export default class PairwiseComputer<P = GeoJsonProperties> {
       }
     }
 
-    return [this.#s1.features[state.bestI], this.#s2.features[state.bestJ]];
+    return [this.s1.features[state.bestI], this.s2.features[state.bestJ]];
   };
 
-  readonly #s1: FeatureCollection<Point, P>;
-  readonly #s2: FeatureCollection<Point, P>;
-  readonly #S1: readonly Vec3[];
-  readonly #S2: readonly Vec3[];
-  readonly root1: Node;
-  readonly root2: Node;
+  private readonly s1: FeatureCollection<Point, P>;
+  private readonly s2: FeatureCollection<Point, P>;
+  private readonly S1: readonly Vec3[];
+  private readonly S2: readonly Vec3[];
+  private readonly root1: Node;
+  private readonly root2: Node;
 }
